0500aa3e9c7cf06d431246c41e845d4eb1e4c031
KavyaJain1206 <kjain2005.kj@gmail.com>
2026-02-18 09:47:30 +0530

    updated readme and code

--- EXP5/MultiLazyLoading/README.md
+++ EXP5/MultiLazyLoading/README.md
@@ -5,6 +5,8 @@ This project demonstrates lazy loading for multiple components and routes in Rea
 ## Demo
 
 ![Multi lazy loading demo](./image.png)
+![Multi lazy loading demo](./image2.png)
+![Multi lazy loading demo](./image3.png)
 
 ## What it shows
 
--- EXP5/MultiLazyLoading/src/components/Contact.jsx
+++ EXP5/MultiLazyLoading/src/components/Contact.jsx
@@ -1,4 +1,10 @@
 function Contact() {
-  return <h2>Contact Page</h2>
+  return (
+    <section>
+      <h2>Contact Page</h2>
+      <p>Email: kjain2005.kj@gmail.com</p>
+      <p>Phone No.: 6378264195</p>
+    </section>
+  )
 }
 export default Contact
